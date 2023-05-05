0eedb08590294fd716732bbd33918a7e5c5cc0c2
Mahalu-Ciprian-FMI <128313829+Mahalu-Ciprian-FMI@users.noreply.github.com>
2023-05-05 09:47:23 +0300

    Update clase.h

--- clase.h
+++ clase.h
@@ -3,24 +3,27 @@
 #include <cstring>
 #include <vector>
 #include <typeinfo>
+#include <exception>
+#include <memory>
+
 class Data
 {
 	int zi;
-	const char* luna;
+	std::string luna;
 	int an;
 public:
 	Data();
-	Data(int zi1, const char* luna1, int an1);
-	Data(Data &data);
+	Data(int zi1, std::string luna1, int an1);
+	Data(const Data& data);
 	~Data();
-	friend std::istream& operator>>(std::istream& in, Data* obj);
+	friend std::istream& operator>>(std::istream& in, Data& obj);
 	friend std::ostream& operator<<(std::ostream& out, Data& obj);
 	void operator=(const Data& obj);
 	int get_zi();
-	const char* get_luna();
+	std::string get_luna();
 	int get_an();
 	void set_zi(int z);
-	void set_luna(char* l);
+	void set_luna(std::string l);
 	void set_an(int a);
 };
 class Angajat
@@ -31,12 +34,8 @@ protected:
 	float salariu;
 	Data data_angajare;
 public:
-	Angajat();
-	Angajat(std::string nume1, std::string prenume1, float salariu1, Data data_angajare1);
-	Angajat(Angajat& angajat);
-	friend std::istream& operator>>(std::istream& in, Angajat* obj);
-	friend std::ostream& operator<<(std::ostream& out, Angajat& obj);
-	void operator=(const Angajat& obj);
+	virtual void citire() = 0;
+	virtual void afisare() = 0;
 	std::string get_nume();
 	std::string get_prenume();
 	float get_salariu();
@@ -45,36 +44,39 @@ public:
 	void set_prenume(std::string p);
 	void set_salariu(float s);
 	void set_data_angajare(Data d);
-	virtual void afisare() = 0;
+	virtual ~Angajat();
 };
-class Part_Time:public Angajat
+class Part_Time : public Angajat
 {
+protected:
 	int nr_ore_zi;
 	Data final_contract;
 public:
+	virtual void citire() override;
+	virtual void afisare() override;
 	Part_Time();
-	Part_Time(int nr_ore_zi, Data final_contract);
-	Part_Time(Part_Time& part);
-	friend std::istream& operator>>(std::istream& in, Part_Time* obj);
-	friend std::ostream& operator<<(std::ostream& out, Part_Time* obj);
-	void operator=(const Part_Time& obj);
+	~Part_Time() override;
 	int get_nr_ore_zi();
 	Data get_final_contract();
 	void set_nr_ore_zi(int nr);
 	void set_final_contract(Data final);
-	void afisare() override;
+
 };
-class Permanent:public Angajat
+class Permanent : public Angajat
 {
+protected:
 	int nr_minori_intretinere;
 public:
+	virtual void citire() override;
+	virtual void afisare() override;
 	Permanent();
-	Permanent(int nr_minori_intretinere);
-	Permanent(Permanent& perm);
-	friend std::istream& operator>>(std::istream& in, Permanent* obj);
-	friend std::ostream& operator<<(std::ostream& out, Permanent& obj);
-	void operator=(const Permanent& obj);
+	~Permanent() override;
 	int get_nr_minori();
 	void set_nr_minori(int nr);
-	void afisare() override;
+};
+
+class Expired : public std::exception
+{
+public:
+	const char* motive();
 };
